fd7dbbdee668b1a23ad0ff4cc45896ea360d98b0
mohit <mohitkhadayat06@gmail.com>
2021-10-05 14:05:33 +0545

    add to cart design done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return(
-        <div>
-            
+  return (
+    <div>
+        <div className="add-to-cart">
+            <img src="https://media.istockphoto.com/vectors/shopping-cart-icon-isolated-on-white-background-vector-id1206806317?k=20&m=1206806317&s=170667a&w=0&h=kEh5VLsTHukWc7xf2BvUs8ssqS_d7vkK0-xU3MDpO7s="/>
         </div>
-    )
-}
+      
+      <div className="cart-wrapper">
+        <div className="img-wrapper item">
+          <img src="https://cdn.vox-cdn.com/thumbor/SJcmPEheS_cbdujd4zbIPTpuXfg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13315959/akrales_181019_3014_0770.jpg" />
+        </div>
+        <div className="text-wrapper item">
+          <span>I-Phone</span>
+          <span>Price: $1000.00</span>
+        </div>
+        <div className="btn-wrapper item">
+          <button>Add to cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
